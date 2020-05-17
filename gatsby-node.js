2c7046004aa4057297727fd8744fc651dd94b505
Motty Jaffe <mottyjaffe@gmail.com>
2020-05-17 22:08:26 +0300

    created all tags page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
 const { slugify } = require("./src/utilities/utilityFunctions")
-const path = require('path')
-const authors = require('./src/utilities/authors')
+const path = require("path")
+const authors = require("./src/utilities/authors")
+const _ = require("lodash")
 
 exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions
@@ -14,40 +15,70 @@ exports.onCreateNode = ({ node, actions }) => {
   }
 }
 
-exports.createPages = ({ actions, graphql}) => {
-    const {createPage} = actions
-    const singlePostTemplate = path.resolve('src/templates/single-post.js')
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
 
-    return graphql(`
-        {
-            allMarkdownRemark {
-                edges{
-                    node{
-                        frontmatter{
-                            author
-                        }
-                        fields{
-                            slug
-                        }
-                    }
-                }
+  const templates = {
+    singlePost: path.resolve("src/templates/single-post.js"),
+    tagsPage: path.resolve("src/templates/tags-page.js"),
+  }
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              author
+              tags
             }
-        } 
-    `).then(res => {
-        if(res.errors) return Promise.reject(res.errors)
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if (res.errors) return Promise.reject(res.errors)
 
-        const posts = res.data.allMarkdownRemark.edges
+    const posts = res.data.allMarkdownRemark.edges
 
-        posts.forEach(({node}) => {
-            createPage({
-                path: node.fields.slug,
-                component: singlePostTemplate,
-                context: {
-                    // Passing slug for template to use to get post
-                    slug: node.fields.slug,
-                    imageUrl: authors.find(x => x.name === node.frontmatter.author).imageUrl
-                }
-            })
-        })
+    posts.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: templates.singlePost,
+        context: {
+          // Passing slug for template to use to get post
+          slug: node.fields.slug,
+          imageUrl: authors.find(x => x.name === node.frontmatter.author)
+            .imageUrl,
+        },
+      })
     })
+
+    // Get all tags
+    let tags = []
+    _.each(posts, edge => {
+      if (_.get(edge, "node.frontmatter.tags")) {
+        tags = tags.concat(edge.node.frontmatter.tags)
+      }
+    })
+
+    let tagPostCounts = {}
+    tags.forEach(tag => {
+      tagPostCounts[tag] = (tagPostCounts[tag] || 0) + 1
+    })
+
+    tags = _.uniq(tags)
+    // create tags page
+    createPage({
+      path: `tags`,
+      component: templates.tagsPage,
+      context: {
+        tags,
+        tagPostCounts,
+      },
+    })
+  })
 }
